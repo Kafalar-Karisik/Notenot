bd147e86d373a1d36fb8a19dffa9d8e0d99365df
Pragmatic Minds Computer <orsmes6145@gmail.com>
2023-05-10 12:19:20 +0200

    Model optimization.

--- notes/models.py
+++ notes/models.py
@@ -7,11 +7,14 @@ import datetime
 # Create your models here.
 
 class Note(models.Model):
-    title = models.CharField(max_length=20, default=" ")
     note = models.CharField(max_length=500)
+    title = models.CharField(max_length=20, default=note, primary_key=True)
     pub_date = models.DateTimeField("date published")
 
     def __str__(self):
+        return self.title
+
+    def obs(self):
         return print(f"{self.title}: {self.note}")
 
     def was_published_recently(self):
