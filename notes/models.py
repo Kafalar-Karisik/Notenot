e81076c04c527923a9d2657eae576d8e221832fc
Ömer Rasim <orsmes6145@gmail.com>
2023-05-26 16:09:22 +0200

    Model edit.

--- notes/models.py
+++ notes/models.py
@@ -16,8 +16,6 @@ class Note(models.Model):
     def __str__(self):
         return self.title
 
-    def obs(self):
-        return print(f"{self.title}: {self.note}")
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
