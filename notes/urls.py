a3c0eabc08f2ead84655a5b59de23b956c22708a
Ömer Rasim <orsmes6145@gmail.com>
2023-05-27 15:06:16 +0200

    urls.py Update.

--- notes/urls.py
+++ notes/urls.py
@@ -1,12 +1,12 @@
-from django.views.generic import TemplateView
 from django.urls import path
 
 from . import views
 
 handler404 = "notes.views.PageNFView"
 
+app_name = 'notes'
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("test/", views.NoteListsView.as_view()),
-]
+]    path("<int:id>/", views.note.as_view(), name="note")
